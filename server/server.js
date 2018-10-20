5341f8e0f5fb1f211f74375c4926b0552f71bccf
Yuriy Rypka <yurarypka@gmail.com>
2018-10-21 02:33:17 +0300

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,16 @@ app.post('/todos', (req, res) => {
     })
 })
 
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then(todos => {
+      res.send({ todos })
+    })
+    .catch(err => {
+      res.status(400).send(err)
+    })
+})
+
 app.listen(process.env.PORT || 3000, () =>
   console.log('Todo Api server started on localhost:3000')
 )
