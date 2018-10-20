afadb315ea0a309640957024ac6788f45ecc3bce
Yuriy Rypka <yurarypka@gmail.com>
2018-10-21 00:35:07 +0300

    validators, types, and default values

--- server/server.js
+++ server/server.js
@@ -14,22 +14,51 @@ const saveTodo = todo =>
 
 const Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    minLength: 1,
+    required: true,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 })
 
 // const newTodo = new Todo({ text: 'Cook dinner' })
 // saveTodo(newTodo)
 
-const otherTodo = new Todo({
-  text: 'Walk a dog',
-  completed: false,
-  completedAt: Date.now()
+// const otherTodo = new Todo({
+//   text: 'Walk a dog',
+//   completed: false,
+//   completedAt: Date.now()
+// })
+// saveTodo(otherTodo)
+
+// const otherTodo = new Todo({ text: ' Edit this todo ' })
+// saveTodo(otherTodo)
+
+const User = mongoose.model('User', {
+  email: {
+    type: String,
+    minLength: 1,
+    required: true,
+    trim: true
+  },
+  createdAt: {
+    type: Number,
+    default: Date.now()
+  }
 })
-saveTodo(otherTodo)
+
+const user = new User({
+  email: 'asd@asd.com'
+})
+user
+  .save()
+  .then(doc => console.log(doc))
+  .catch(err => console.log(err))
